e7ac10408b83dab5907138ee784c6d46a8bde877
minjeong <ehdro418@naver.com>
2023-04-27 12:16:52 +0900

    complete signup

--- users/models.py
+++ users/models.py
@@ -1,3 +1,79 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, name, gender, age, introduction, password=None):
+        if not email:
+            raise ValueError("Users must have an email address")
+
+        user = self.model(
+            email=self.normalize_email(email),
+            name=name,
+            gender=gender,
+            age=age,
+            introduction=introduction,
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None):
+        user = self.create_user(
+            email=email,
+            password=password,
+            name="관리자",
+            gender="non-choice",
+            age="0",
+            introduction="관리자 계정입니다"
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser):
+    GENDER_CHOICES = (
+        ("male", "남자"),
+        ("female", "여자"),
+        ("unknown", "모름"),
+        ("non-choice", "선택하지 않음")
+    )
+
+    email = models.EmailField(
+        verbose_name="email address",
+        max_length=255,
+        unique=True,
+    )
+    name = models.CharField("이름", max_length=20)
+    gender = models.CharField("성별", max_length=20, choices=GENDER_CHOICES)
+    age = models.IntegerField(
+        "나이", validators=[MinValueValidator(0), MaxValueValidator(120)])
+    introduction = models.TextField("자기소개", blank=True)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        # Simplest possible answer: All admins are staff
+        return self.is_admin
--- users/views.py
+++ users/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import status, permissions
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.generics import get_object_or_404
+from users.serializers import UserSerializer
 
 # Create your views here.
+
+
+class SignupView(APIView):
+    def post(self, request):
+        """회원가입"""
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "가입완료!"}, status=status.HTTP_201_CREATED)
+        else:
+            return Response({"message": f"${serializer.errors}"}, status=status.HTTP_400_BAD_REQUEST)
